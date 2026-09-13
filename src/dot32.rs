aa574b348a5ed9417edec863847281c78c39d42f
m0nten <just.m0nten@proton.me>
2026-09-13 10:09:17 +0300

    feat: add par_dot_avx2() and par_dot_scalar() functions

--- src/dot32.rs
+++ src/dot32.rs
@@ -1,5 +1,19 @@
 use std::arch::x86_64::*;
 use std::ops::Mul;
+use rayon::iter::{IndexedParallelIterator, ParallelIterator};
+use rayon::slice::ParallelSlice;
+
+pub const DEFAULT_DOT32_CHUNK: usize = 16 * 1024;
+
+#[target_feature(enable = "avx2", enable = "fma")]
+pub unsafe fn par_dot_avx2(x: &[f32], y: &[f32], chunk_size: usize) -> f32 {
+    x.par_chunks(chunk_size).zip(y.par_chunks(chunk_size)).map(|(x,y )| unsafe {dot_avx2(x, y)}).sum()
+}
+
+#[inline(always)]
+pub fn par_dot_scalar(x: &[f32], y: &[f32], chunk_size: usize) -> f32 {
+    x.par_chunks(chunk_size).zip(y.par_chunks(chunk_size)).map(|(x,y )| dot_scalar(x, y)).sum()
+}
 
 /// # Safety
 /// The length of vector X and the length of vector Y must not differ.
